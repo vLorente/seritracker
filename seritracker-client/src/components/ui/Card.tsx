f240544621e563b3683281c07737d1e893a10c34
vLorente <valenlj7@gmail.com>
2024-03-12 06:10:13 +0100

    Update Card component image width full

--- seritracker-client/src/components/ui/Card.tsx
+++ seritracker-client/src/components/ui/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({ image, title, description }: Props) {
 		<article class="group card bg-gray-200 transition-all duration-300 hover:scale-105 hover:contrast-[105%] dark:bg-gray-900">
 			<div class="card">
 				<figure class="group-hover:brightness-[20%]">
-					<img src={image} alt={title} />
+					<img class="w-full" src={image} alt={title} />
 				</figure>
 				<p class="invisible absolute bottom-0 left-0 right-0 px-2 text-white group-hover:visible group-hover:animate-fade-in-down">
 					{description}
