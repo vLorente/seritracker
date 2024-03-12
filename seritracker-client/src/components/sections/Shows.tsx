3847f3333ef677d0c097c431b1b48839e4978e89
vLorente <valenlj7@gmail.com>
2024-03-12 06:07:39 +0100

    Add Show Section

---
+++ seritracker-client/src/components/sections/Shows.tsx
@@ -0,0 +1,31 @@
+import Card from "@components/ui/Card.tsx"
+import useFetchMal from "@hooks/useFetchMal.ts"
+
+export default function Shows() {
+	const { data, error, loading } = useFetchMal()
+
+	return (
+		<section className="max-w-8xl mx-auto px-20 py-20">
+			<div class="grid grid-cols-1 content-start gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+				{data &&
+					data!.data.map(({ title, synopsis, images }) => (
+						<Card
+							title={title}
+							description={synopsis}
+							image={images.webp.large_image_url}
+						/>
+					))}
+				{loading && (
+					<>
+						<div className="flex w-52 flex-col gap-4">
+							<div className="skeleton h-32 w-full"></div>
+							<div className="skeleton h-4 w-28"></div>
+							<div className="skeleton h-4 w-full"></div>
+							<div className="skeleton h-4 w-full"></div>
+						</div>
+					</>
+				)}
+			</div>
+		</section>
+	)
+}
