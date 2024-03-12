d87ffb78ce9dfb9e2a588cb992f3414b747a8f4f
vLorente <valenlj7@gmail.com>
2024-03-12 14:28:30 +0100

    Change Card class to className

--- seritracker-client/src/components/ui/Card.tsx
+++ seritracker-client/src/components/ui/Card.tsx
@@ -6,21 +6,23 @@ interface Props {
 
 export default function Card({ image, title, description }: Props) {
 	return (
-		<article class="group card bg-gray-200 transition-all duration-300 hover:scale-105 hover:contrast-[105%] lg:w-full 2xl:h-full dark:bg-gray-900">
-			<div class="card overflow-hidden text-clip">
-				<figure class="group-hover:brightness-[20%]">
-					<img class="w-full" src={image} alt={title} />
+		<article className="group card bg-gray-200 transition-all duration-300 hover:scale-105 hover:contrast-[105%] lg:w-full 2xl:h-full dark:bg-gray-900">
+			<div className="card overflow-hidden text-clip">
+				<figure className="group-hover:brightness-[20%]">
+					<img className="w-full" src={image} alt={title} />
 				</figure>
-				<p class="invisible absolute bottom-0 left-0 right-0 px-3 text-white group-hover:visible group-hover:animate-fade-in-down">
+				<p className="invisible absolute bottom-0 left-0 right-0 px-3 text-white group-hover:visible group-hover:animate-fade-in-down">
 					{description}
 				</p>
 			</div>
-			<div class="card-body justify-end">
-				<h2 class="card-title">{title}</h2>
-				<div class="card-actions justify-end">
-					<button class="btn btn-active">Viendo</button>
-					<button class="btn btn-neutral">Pendiente</button>
-					{/* <button class="btn bg-lime-600 text-gray-300">Vista</button> */}
+			<div className="card-body justify-end">
+				<h2 className="card-title">{title}</h2>
+				<div className="card-actions join-horizontal justify-end">
+					<button className="btn bg-lime-600 text-gray-300 hover:bg-lime-800">
+						Viendo
+					</button>
+					<button className="btn btn-neutral">Pendiente</button>
+					{/* <button className="btn bg-lime-600 text-gray-300">Vista</button> */}
 				</div>
 			</div>
 
