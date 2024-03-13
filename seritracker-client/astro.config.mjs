e0842cec79677cdf803663b41d31c0410e8e19f1
vLorente <valenlj7@gmail.com>
2024-03-13 03:34:38 +0100

    Deploy to Vercel Config

--- seritracker-client/astro.config.mjs
+++ seritracker-client/astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from "astro/config"
-
 import preact from "@astrojs/preact"
 import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		preact(),
-		tailwind(),
-	],
-	output: "hybrid"
+	integrations: [preact(), tailwind()],
+	output: "hybrid",
+	adapter: vercel()
 })
