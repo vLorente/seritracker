2727625dbf80d85eacd9d7a2214afae2e71375ab
vLorente <valenlj7@gmail.com>
2024-03-13 16:22:37 +0100

    The Movie DB API uses

--- seritracker-client/src/env.d.ts
+++ seritracker-client/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly SECRET_TMDB_APIKEY: string
+	readonly PUBLIC_TMDBAPI: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
