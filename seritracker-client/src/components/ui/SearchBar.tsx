8b8f1b49253d7fca55ada0acd59498f3847de1f3
vLorente <valenlj7@gmail.com>
2024-03-15 14:50:13 +0100

    Update SerachBar adding usekey to focus on input

--- seritracker-client/src/components/ui/SearchBar.tsx
+++ seritracker-client/src/components/ui/SearchBar.tsx
@@ -1,5 +1,6 @@
 import useDebounce from "@hooks/useDebounce.ts"
-import { useEffect, useState } from "preact/hooks"
+import useKey from "@hooks/useKey"
+import { useEffect, useRef, useState } from "preact/hooks"
 
 interface SearchBarProps {
 	className?: string
@@ -27,16 +28,36 @@ export default function SearchBar({
 		setSearchValue(value)
 	}
 
+	// Manejar el evento de teclado # + K para hacer focus en el Input
+	const type = "keypress"
+	const event = new KeyboardEvent(type, {
+		code: "KeyK",
+		ctrlKey: false,
+		altKey: false,
+		shiftKey: true,
+		metaKey: false,
+	})
+
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	const handleKeyPress = () => {
+		inputRef?.current?.focus()
+	}
+
+	useKey(type, event, handleKeyPress)
+
 	return (
 		<label className={combinedClassName}>
 			<input
-				type="text"
+				id="searchbar-input"
+				ref={inputRef}
+				type="search"
 				className="md: input-ghost w-full grow border-none"
 				placeholder="Buscar"
 				value={searchValue}
 				onInput={handleOnChange}
 			/>
-			<kbd className="kbd kbd-sm text-lime-600">Ctrl</kbd>
+			<kbd className="kbd kbd-sm text-lime-600">Shift</kbd>
 			<kbd className="kbd kbd-sm text-lime-600">K</kbd>
 		</label>
 	)
